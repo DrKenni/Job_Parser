c1d963363e69eab0495931a617761918693861b1
drkenni <tanyromachka1@mail.ru>
2023-06-14 15:57:15 +0300

    Added SJ class

--- classes.py
+++ classes.py
@@ -26,18 +26,23 @@ class HeadHunterAPI(AbstractClass):
     def get_formatted_vacancies(data):
         formatted_vacancies = []
         for vacancy in data:
+            if vacancy['salary'] is None:
+                currency, salary_from, salary_to = None, 0, 0
+            else:
+                currency, salary_from, salary_to = \
+                    vacancy['salary']['currency'], vacancy['salary']['from'], vacancy['salary']['to']
             formatted_vacancies.append({
                 'api': 'HeadHunter',
                 'title': vacancy['name'],
                 'url': vacancy['alternate_url'],
-                'salary_from': vacancy['salary']['from'],
-                'salary_to': vacancy['salary']['to'],
-                'currency': vacancy['salary']['currency'],
+                'salary_from': salary_from,
+                'salary_to': salary_to,
+                'currency': currency,
                 'employer': vacancy['employer']['name'],
             })
         return formatted_vacancies
 
-    def get_vacancies(self, search_query, top_n):
+    def get_reqest(self, search_query):
         params = {'text': search_query,
                   'page': 0,
                   'per_page': 100}
@@ -45,26 +50,66 @@ class HeadHunterAPI(AbstractClass):
         if response.status_code != 200:
             raise ParsingError
         else:
-            data = response.json()['items']
+            return response.json()['items']
+
+    def get_vacancies(self, search_query, top_n):
+        try:
+            data = self.get_reqest(search_query)
             self.__vacancies.extend(self.get_formatted_vacancies(data))
             return self.vacancies
-                # print(item['profession'])
-                # vacancies = Vacancy(item['name'], item['url'], item['salary'], item['snippet']['requirement'])
+        except ParsingError:
+            raise ParsingError('Ошибка получения данных с HeadHunter')
 
-hh = HeadHunterAPI()
-hh.get_vacancies(['Python'], 5)
+# hh = HeadHunterAPI()
+# pprint(hh.get_vacancies('Python', 5))
+
 
 class SuperJobAPI(AbstractClass):
     """Получает вакансии с SuperJob"""
     header = {"X-Api-App-Id": os.getenv('SJ_API_KEY')}
 
     def __init__(self):
-        pass
+        self.__vacancies = []
+
+    @property
+    def vacancies(self):
+        return self.__vacancies
+
+    @staticmethod
+    def get_formatted_vacancies(data):
+        formatted_vacancies = []
+        for vacancy in data:
+            formatted_vacancies.append({
+                'api': 'SuperJob',
+                'title': vacancy['profession'],
+                'url': vacancy['link'],
+                'salary_from': vacancy['payment_from'],
+                'salary_to': vacancy['payment_to'],
+                'currency': vacancy['currency'],
+                'employer': vacancy['firm_name'],
+            })
+        return formatted_vacancies
+
+    def get_request(self, search_query):
+        params = {'keyword': search_query,
+                  'page': 0,
+                  'count': 100}
+        response = get('https://api.superjob.ru/2.0/vacancies/', params=params, headers=self.header)
+        if response.status_code != 200:
+            raise ParsingError
+        else:
+            return response.json()['objects']
 
     def get_vacancies(self, search_query, top_n):
-        pass
+        try:
+            data = self.get_request(search_query)
+            self.__vacancies.extend(self.get_formatted_vacancies(data))
+            return self.vacancies
+        except ParsingError:
+            raise ParsingError('Ошибка получения данных с SuperJob')
 
-
+sj = SuperJobAPI()
+pprint(sj.get_vacancies('Python', 5))
 class Vacancy:
     """Создает экземпляры класса для работы с вакансиями"""
     __slots__ = ('name', 'url', 'salary', 'conditions')
