55b4f05bf028dc6e3d4fb74e249a48906d25067e
drkenni <tanyromachka1@mail.ru>
2023-06-17 12:35:39 +0300

    Added docstrings

--- classes.py
+++ classes.py
@@ -11,41 +11,59 @@ class AbstractClass(ABC):
         pass
 
     @abstractmethod
-    def get_vacancies(self, search_query, top_n):
+    def get_vacancies(self, search_query):
         pass
 
 
-class HeadHunterAPI(AbstractClass):
+class MixinSalary:
+    @staticmethod
+    def formatted_salary(s_from, s_to, currency):
+        new_format = [0, 0, 0]
+        new_format[0] = s_from if s_from != 0 else 0
+        new_format[1] = s_to if s_to != 0 else 0
+        new_format[2] = currency if s_to or s_from != 0 or None else 'Не указана'
+        return new_format
+
+
+class HeadHunterAPI(AbstractClass, MixinSalary):
     """Получает вакансии с HeadHunter"""
     
     def __init__(self):
         self.__vacancies = []
 
+    def __str__(self):
+        return "HeadHunter"
+
     @property
     def vacancies(self):
         return self.__vacancies
 
-    @staticmethod
-    def get_formatted_vacancies(data):
+    def get_formatted_vacancies(self, data):
+        """Собирает нужные данные в список сo словарем
+        data: список json с вакансиями с сайта
+        return: словарь с нужными ключами"""
         formatted_vacancies = []
         for vacancy in data:
+
             if vacancy['salary'] is None:
-                currency, salary_from, salary_to = None, 0, 0
+                s_from, s_to, currency = 0, 0, 'Не указана'
             else:
-                currency, salary_from, salary_to = \
-                    vacancy['salary']['currency'], vacancy['salary']['from'], vacancy['salary']['to']
+                s_from, s_to, currency = self.formatted_salary(
+                    vacancy['salary']['from'], vacancy['salary']['to'], vacancy['salary']['currency'])
             formatted_vacancies.append({
                 'api': 'HeadHunter',
                 'title': vacancy['name'],
                 'url': vacancy['alternate_url'],
-                'salary_from': salary_from,
-                'salary_to': salary_to,
+                'salary_from': s_from,
+                'salary_to': s_to,
                 'currency': currency,
                 'employer': vacancy['employer']['name'],
             })
         return formatted_vacancies
 
     def get_request(self, search_query):
+        """Получает список json с вакансиями с сайта
+        search_query: Слово по которому идет поиск вакансий"""
         params = {'text': search_query,
                   'page': 0,
                   'per_page': 100}
@@ -55,7 +73,10 @@ class HeadHunterAPI(AbstractClass):
         else:
             return response.json()['items']
 
-    def get_vacancies(self, search_query, top_n):
+    def get_vacancies(self, search_query):
+        """Возвращает отфильтрованный по атрибуту список с вакансиями
+        search_query: Слово по которому идет поиск вакансий
+        return: список класса отфильтрованный по атрибуту с вакансиями"""
         try:
             data = self.get_request(search_query)
             self.__vacancies.extend(self.get_formatted_vacancies(data))
@@ -63,37 +84,43 @@ class HeadHunterAPI(AbstractClass):
         except ParsingError:
             raise ParsingError('Ошибка получения данных с HeadHunter')
 
-# hh = HeadHunterAPI()
-# pprint(hh.get_vacancies('Python', 5))
 
-
-class SuperJobAPI(AbstractClass):
+class SuperJobAPI(AbstractClass, MixinSalary):
     """Получает вакансии с SuperJob"""
     header = {"X-Api-App-Id": os.getenv('SJ_API_KEY')}
 
     def __init__(self):
         self.__vacancies = []
 
+    def __str__(self):
+        return "SuperJob"
+
     @property
     def vacancies(self):
         return self.__vacancies
 
-    @staticmethod
-    def get_formatted_vacancies(data):
+    def get_formatted_vacancies(self, data):
+        """Собирает нужные данные в список сo словарем
+        data: список json с вакансиями с сайта
+        return: словарь с нужными ключами"""
         formatted_vacancies = []
         for vacancy in data:
+            s_from, s_to, currency = self.formatted_salary(
+                vacancy['payment_from'], vacancy['payment_to'], vacancy['currency'])
             formatted_vacancies.append({
                 'api': 'SuperJob',
                 'title': vacancy['profession'],
                 'url': vacancy['link'],
-                'salary_from': vacancy['payment_from'],
-                'salary_to': vacancy['payment_to'],
-                'currency': vacancy['currency'],
+                'salary_from': s_from,
+                'salary_to': s_to,
+                'currency': currency,
                 'employer': vacancy['firm_name'],
             })
         return formatted_vacancies
 
     def get_request(self, search_query):
+        """Получает список json с вакансиями с сайта
+        search_query: Слово по которому идет поиск вакансий"""
         params = {'keyword': search_query,
                   'page': 0,
                   'count': 100}
@@ -103,16 +130,16 @@ class SuperJobAPI(AbstractClass):
         else:
             return response.json()['objects']
 
-    def get_vacancies(self, search_query, top_n):
+    def get_vacancies(self, search_query):
+        """Возвращает отфильтрованный по атрибуту список с вакансиями
+        search_query: Слово по которому идет поиск вакансий
+        return: список класса отфильтрованный по атрибуту с вакансиями"""
         try:
             data = self.get_request(search_query)
             self.__vacancies.extend(self.get_formatted_vacancies(data))
             return self.vacancies
         except ParsingError:
             raise ParsingError('Ошибка получения данных с SuperJob')
-
-# sj = SuperJobAPI()
-# pprint(sj.get_vacancies('Python', 5))
 
 
 class Vacancy:
@@ -133,17 +160,11 @@ class Vacancy:
 
     def __str__(self):
         salary_from = f'От {self.salary_from}' if self.salary_from else ''
-        salary_to = f'От {self.salary_to}' if self.salary_to else ''
-        if self.salary_from and self.salary_to is 0:
-            self.salary_to = "Не указана"
-            self.salary_from = ''
-            self.currency = ''
-        return f'Компания: {self.employer}\nВакансия: {self.title}' \
-               f'Зарплата: {salary_from} {salary_to} {self.currency}' \
-               f'Url: {self.url}'
-
-    # def __getitem__(self, item):
-    #     return item
+        salary_to = f'До {self.salary_to}' if self.salary_to else ''
+        return f'{self.api}\n' \
+               f'Компания: {self.employer}\nВакансия: {self.title}\n' \
+               f'Зарплата: {salary_from} {salary_to} {self.currency}\n' \
+               f'Url: {self.url}\n'
 
     def __gt__(self, other):
         if other.salary_from is None:
@@ -151,10 +172,3 @@ class Vacancy:
         elif self.salary_from is None:
             return False
         return self.salary_from > other.salary_from
-
-    def __lt__(self, other):
-        if other.salary_from is None:
-            return True
-        elif self.salary_from is None:
-            return False
-        return self.salary_from < other.salary_from
--- connector.py
+++ connector.py
@@ -11,51 +11,39 @@ class Connector(ABC):
         pass
 
     @abstractmethod
-    def delete_vacancy(self, query):
+    def select(self):
         pass
 
     @abstractmethod
-    def select(self):
+    def delete_save_file(self):
         pass
 
 
 class JSONSaver:
     def __init__(self, search_query, vacancy_list):
         self.__file_to_save = f'{search_query.title()}.json'
-        folder_path = path.abspath("Job_Parser/data")
-        self.file_path = path.join(folder_path, self.__file_to_save)
+        self.file_path = path.join('data', self.__file_to_save)
         self.insert(vacancy_list)
 
     def insert(self, vacancy):
-        with open(self.file_path, 'w') as f:
+        """Создает фаил в котором будет храниться информация в json формате"""
+        with open(self.file_path, 'w', encoding='windows-1251') as f:
             json.dump(vacancy, f, indent=4, ensure_ascii=False)
 
     def select(self):
+        """Создает экземпляры класса Vacancy
+        return: список экземпляров класса Vacancy"""
         with open(self.file_path, 'r') as f:
             file_data = json.load(f)
-            vacancies = [Vacancy(i['api'], i['title'], i['url'],
-                                 i['salary_from'], i['salary_to'],
-                                 i['currency'], i['employer']) for i in file_data]
+            vacancies = []
+
+            for i in file_data:
+                vacancy = Vacancy(i['api'], i['title'], i['url'], i['salary_from'],
+                                  i['salary_to'], i['currency'], i['employer'])
+                vacancies.append(vacancy)
+
             return vacancies
 
-    def get_vacancies_by_salary(self, salary_from, salary_to=None):
-        filtered_list = []
-
-        for vacancy in self.select():
-            if salary_to is None:
-                if vacancy.salary_from and vacancy.salary_to == 0:
-                    continue
-                elif vacancy.salary_to != 0:
-                    if salary_from < vacancy.salary_to:
-                        filtered_list.append(vacancy)
-                else:
-                    if salary_from < vacancy.salary_from:
-                        filtered_list.append(vacancy)
-                    continue
-        if len(filtered_list) == 0:
-            return 'К сожалению вакансий от такой суммы нет.'
-        else:
-            return filtered_list
-
     def delete_save_file(self):
+        """Удаляет фаил в который сохраняли json данные"""
         remove(self.file_path)
