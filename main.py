68f524c90066acf6825d82011e57d656d48c2b16
drkenni <tanyromachka1@mail.ru>
2023-06-17 12:32:40 +0300

    Added core code

--- main.py
+++ main.py
@@ -1,34 +1,59 @@
-from utils import filter_vacancies, sort_vacancies, get_top_vacancies, print_vacancies
-from classes import Vacancy, HeadHunterAPI, SuperJobAPI
+from classes import HeadHunterAPI, SuperJobAPI
 from connector import JSONSaver
+from utils import sort_vacancies, get_top_vacancies, choosing_actions, finish
+
 # Создание экземпляра класса для работы с API сайтов с вакансиями
 hh_api = HeadHunterAPI()
 sj_api = SuperJobAPI()
-
-# Сохранение информации о вакансиях в файл
-json_saver = JSONSaver()
-json_saver.add_vacancy(vacancy)
-json_saver.get_vacancies_by_salary("100 000-150 000 руб.")
-json_saver.delete_vacancy(vacancy)
 
 
 # Функция для взаимодействия с пользователем
 def user_interaction():
     platforms = [hh_api, sj_api]
-    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    search_query = input("Введите ключевое слово для фильтрации вакансий: ").split()
+    search_query = input("Введите ключевое слово для фильтрации вакансий: ")
+
     # Получение вакансий с разных платформ
-    hh_vacancies = hh_api.get_vacancies(search_query, top_n)
-    superjob_vacancies = sj_api.get_vacancies(search_query, top_n)
-    filtered_vacancies = filter_vacancies([hh_vacancies, superjob_vacancies])
+    vacancy_list = []
+    for platform in platforms:
+        vacancy_list.extend(platform.get_vacancies(search_query))
+        print(f'На {str(platform)} найдено {len(platform.vacancies)} вакансий.')
 
-    if not filtered_vacancies:
-        print("Нет вакансий, соответствующих заданным критериям.")
-        return
+    # Сохранение вакансий в отдельный фаил
+    connector = JSONSaver(search_query, vacancy_list)
 
-    sorted_vacancies = sort_vacancies(filtered_vacancies)
-    top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-    print_vacancies(top_vacancies)
+    # Интерфейс взаимодействия с пользователем
+    while True:
+        user_command = str(input('\n1 - Вывести список вакансий\n'
+                                 '2 - Отсортировать вакансии по ЗП\n'
+                                 '3 - Вывести топ вакансий по ЗП\n'
+                                 '0 - Завершить работу\n'))
+
+        if user_command == '1':
+            vacancies = connector.select()
+            for vacancy in vacancies:
+                print(vacancy, end='\n')
+            choosing_actions(connector)
+
+        elif user_command == '2':
+            while True:
+                sort = sort_vacancies(connector.select())
+                for v in sort:
+                    print(v)
+                choosing_actions(connector)
+
+        elif user_command == '3':
+            top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+            vacancies = get_top_vacancies(connector.select(), top_n)
+            for vacancy in vacancies:
+                print(vacancy)
+            choosing_actions(connector)
+
+        elif user_command == '0':
+            finish(connector)
+
+        else:
+            print('Такой команды нет!')
+            continue
 
 
 if __name__ == "__main__":
